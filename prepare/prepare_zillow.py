8e5320fede0b7063d5f744203dea46c233d3ca8b
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2021-04-04 23:58:36 -0500

    Updated wrangle_zillow to return train, validate, test samples.

--- prepare/prepare_zillow.py
+++ prepare/prepare_zillow.py
@@ -1,11 +1,12 @@
 import env
 import utilities as utils
+import pandas as pd
 
 def drop_unnecessary_columns(zillow_df):
     df = zillow_df.copy()
     
     df = utils.handle_missing_values(df, .6, .4)
-    drop_cols = ['id', 'parcelid', 'finishedsquarefeet12', 'fullbathcnt', 'propertylandusetypeid', 'heatingorsystemtypeid', 'censustractandblock']
+    drop_cols = ['id', 'parcelid', 'finishedsquarefeet12', 'fullbathcnt', 'propertylandusetypeid', 'heatingorsystemtypeid', 'censustractandblock', 'rawcensustractandblock', 'regionidzip', 'assessmentyear', 'regionidcounty', 'yearbuilt']
     
     return df.drop(columns=drop_cols)
 
@@ -24,5 +25,32 @@ def handle_missing_zillow_values(zillow_df):
     df['structuretaxvaluedollarcnt'].fillna(value=136399.0, inplace=True)
     df['heatingorsystemdesc'].fillna(value='Central', inplace=True)
     df['taxamount'].fillna(value=4447.63, inplace=True)
+    df['taxvaluedollarcnt'].fillna(value=358879, inplace=True)
+    df['landtaxvaluedollarcnt'].fillna(value=203174, inplace=True)
     
     return df
+
+def rename_zillow_columns(zillow_df):
+    df = zillow_df.copy()
+    
+    rename_dict = {'bathroomcnt' : 'bathrooms', 'bedroomcnt' : 'bedrooms', 'buildingqualitytypeid' : 'build_quality', 'calculatedbathnbr' : 'fractional_bathrooms', 'calculatedfinishedsquarefeet' : 'sqft', 'lotsizesquarefeet' : 'lot_size', 'propertycountylandusecode' : 'land_use_code', 'propertyzoningdesc' : 'zoning_desc', 'regionidcity' : 'city_id', 'roomcnt' : 'rooms', 'unitcnt' : 'units', 'structuretaxvaluedollarcnt' : 'structure_tax_value', 'taxvaluedollarcnt' : 'tax_value', 'landtaxvaluedollarcnt' : 'land_tax_value', 'taxamount' : 'tax_amount', 'logerror' : 'error', 'transactiondate' : 'transaction_date', 'heatingorsystemdesc' : 'heat_system_desc', 'propertylandusedesc' : 'property_land_use_desc'}
+    
+    return df.rename(columns=rename_dict)
+
+def encode_zillow_categoricals(zillow_df):
+    df = zillow_df.copy()
+
+    fips_dummies = pd.get_dummies(df.fips, dummy_na=False, drop_first=True)
+    heat_dummies = pd.get_dummies(df.heat_system_desc, dummy_na=False, drop_first=True)
+    prop_use_dummies = pd.get_dummies(df.property_land_use_desc, dummy_na=False, drop_first=True)
+    
+    return pd.concat([df, fips_dummies, heat_dummies, prop_use_dummies], axis=1)
+
+def add_features(zillow_df):
+    df = zillow_df.copy()
+    
+    df['age'] = 2017 - df['yearbuilt']
+    
+    return df
+    
+    
--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -1,12 +1,16 @@
 import env
+import utilities as utils
 
 from acquire.acquire_zillow import acquire_zillow_df
-from prepare.prepare_zillow import drop_unnecessary_columns, handle_missing_zillow_values
+from prepare.prepare_zillow import drop_unnecessary_columns, handle_missing_zillow_values, rename_zillow_columns, encode_zillow_categoricals, add_features
 
 def wrangle_zillow_data():
     zillow_df = acquire_zillow_df()
     
-    zillow_df = drop_unnecessary_columns(zillow_df)
     zillow_df = handle_missing_zillow_values(zillow_df)
+    zillow_df = rename_zillow_columns(zillow_df)
+    zillow_df = add_features(zillow_df)
+    zillow_df = encode_zillow_categoricals(zillow_df)
+    zillow_df = drop_unnecessary_columns(zillow_df)
     
-    return zillow_df
+    return utils.split_dataframe_continuous_target(zillow_df, 'error')
